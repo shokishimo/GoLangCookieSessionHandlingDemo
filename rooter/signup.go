8a93c8d4ca38091c7eee90f62656afee40134953
shokishimo <shokishimo@gmail.com>
2023-01-21 11:38:30 -0600

    add code to redirect and add some TODO

--- rooter/signup.go
+++ rooter/signup.go
@@ -7,17 +7,19 @@ import (
 	"net/http"
 )
 
+// SignUpHandler handles the api endpoint '/signup'
 func SignUpHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
+	if r.Method == http.MethodGet { // handle GET method
 		signUpGet(w)
-	} else if r.Method == http.MethodPost {
+	} else if r.Method == http.MethodPost { // handle POST method
 		signUpPost(w, r)
-	} else {
+	} else { // others
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 	return
 }
 
+// signUpGet return signup.html file
 func signUpGet(w http.ResponseWriter) {
 	tmpl, err := template.ParseFiles("static/public/signup.html")
 	if err != nil {
@@ -28,19 +30,29 @@ func signUpGet(w http.ResponseWriter) {
 	tmpl.Execute(w, nil)
 }
 
+// signUpPost save a user signed up
 func signUpPost(w http.ResponseWriter, r *http.Request) {
 	user := model.User{
 		Username: r.FormValue("username"),
 		Password: r.FormValue("password"),
 	}
-	// For debugging
-	// fmt.Fprintf(w, "Username: %s, Password: %s", user.Username, user.Password)
 
+	// TODO: Add validations to input username and password
+
+	// TODO: Hash the password
+
+	// save the user
 	err := model.SaveUser(user)
 	if err != nil {
 		fmt.Fprint(w, err.Error())
 		return
 	}
 
+	// TODO: Create a cookie session for the user
+
+	// success log
 	fmt.Fprint(w, "successfully inserted the user")
+
+	// Redirect to account home page
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
