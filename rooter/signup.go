5a8c18dd1d3ea2558226bc13e388a6901bce4e8a
shokishimo <shokishimo@gmail.com>
2023-01-21 07:27:46 -0600

    Created a signup page

--- rooter/signup.go
+++ rooter/signup.go
@@ -1,6 +1,31 @@
 package rooter
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func SignUpHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		signUpGet(w)
+	} else if r.Method == http.MethodPost {
+		signUpPost(w, r)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+	return
+}
+
+func signUpGet(w http.ResponseWriter) {
+	tmpl, err := template.ParseFiles("static/public/signup.html")
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	tmpl.Execute(w, nil)
+}
+
+func signUpPost(w http.ResponseWriter, r *http.Request) {
+
 }
