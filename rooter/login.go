26f5d44ed7bd842caf6f4d68a38321a5d19b548e
shokishimo <shokishimo@gmail.com>
2023-01-23 16:49:22 -0600

    Implemented the process when a user failed to login

--- rooter/login.go
+++ rooter/login.go
@@ -2,6 +2,7 @@ package rooter
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/shokishimo/OneTap/db"
 	user "github.com/shokishimo/OneTap/model"
@@ -10,16 +11,28 @@ import (
 	"net/http"
 )
 
+type loginError struct {
+	DidFail bool
+	Fail    string
+}
+
 // LoginHandler handles both get and post method
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("login")
 	if r.Method == http.MethodGet { // handle GET method
-		loginGet(w)
+		res := r.URL.Query().Get("error")
+		if res == "invalid_credentials" {
+			data := loginError{DidFail: true, Fail: "Invalid username or password"}
+			loginGet(w, true, data)
+		} else {
+			loginGet(w, false, loginError{})
+		}
 	} else if r.Method == http.MethodPost { // handle POST method
 		err := loginPost(w, r)
 		if err != nil {
 			fmt.Println("login failed")
-			// Redirect to account home page
-			http.Redirect(w, r, "/loginFail", http.StatusSeeOther)
+			// Redirect to login page again
+			http.Redirect(w, r, "/login?error=invalid_credentials", http.StatusSeeOther)
 		}
 	} else { // others
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -28,14 +41,18 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // loginGet returns a login html page
-func loginGet(w http.ResponseWriter) {
+func loginGet(w http.ResponseWriter, didFail bool, data loginError) {
 	tmpl, err := template.ParseFiles("static/public/login.html")
 	if err != nil {
 		w.Write([]byte(err.Error()))
 		return
 	}
 
-	tmpl.Execute(w, nil)
+	if !didFail {
+		tmpl.Execute(w, nil)
+	} else {
+		tmpl.Execute(w, data)
+	}
 }
 
 // loginPost handles login
@@ -63,14 +80,20 @@ func loginPost(w http.ResponseWriter, r *http.Request) error {
 	// update the user in the database if the user exists
 	filter := bson.M{"username": username, "password": password}
 	update := bson.M{"$set": bson.M{"sessionid": user.Hash(sessionID)}}
-	_, err = collection.UpdateOne(context.TODO(), filter, update)
+	result, err := collection.UpdateOne(context.TODO(), filter, update)
 
-	// when the user with the username and password not found
+	// when an error happened in the transaction
 	if err != nil {
-		fmt.Println("User not found")
+		fmt.Println("An error occurred when login check to the database")
 		return err
 	}
+	// when the user with the username and password not found
+	if result.MatchedCount == 0 {
+		fmt.Println("user not found")
+		return errors.New("user not found")
+	}
 
+	// when the user is found
 	// save the cookie in the client browser
 	user.SetCookie(w, sessionID)
 
