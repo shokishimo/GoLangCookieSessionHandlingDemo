36a81adca0e513c715ff04fe398b816832078b18
shokishimo <shokishimo@gmail.com>
2023-01-29 12:08:10 -0600

    add delete app button and it's functionality. Modify the add app func code and make it simpler

--- static/js/createNewProject.js
+++ static/js/createNewProject.js
@@ -1,61 +1,30 @@
-let count = 2;
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("add-another").addEventListener("click", function(){
-        // Create the form fields
-        const p = document.createElement("p")
-        p.innerHTML = "App" + count++;
+    const appContainer = document.getElementById("addContainer");
+    const addAppButton = document.getElementById("add-another");
 
-        const osLabel = document.createElement("label");
-        osLabel.setAttribute("for", "os");
-        osLabel.innerHTML = "Operating System: ";
-        const osSelect = document.createElement("select");
-        osSelect.setAttribute("id", "os");
-        osSelect.setAttribute("name", "os");
+    addAppButton.addEventListener("click", () => {
+        // create a new app element
+        const appElement = document.createElement("div");
+        appElement.innerHTML += `
+                <p class="app head">App</p>
+                <label for="os">Operating System: </label>
+                <select id="os" name="os">
+                    <option value="Windows">Mac</option>
+                    <option value="Mac">Windows</option>
+                </select><br><br>
+                <label for="browser">Browser Name (if it's a web app): </label>
+                <input type="text" id="browser" name="browser" size="25" placeholder="otherwise, keep this blank"><br><br>
+                <label for="url">URL: </label>
+                <input type="text" id="url" name="url" placeholder="or path to the app"><br><br>
+                <button class="delete-app-button">Delete</button><br><br><hr>
+        `
+        // add the new app element to the app container
+        appContainer.appendChild(appElement);
 
-        const osOption1 = document.createElement("option");
-        osOption1.setAttribute("value", "Windows");
-        osOption1.innerHTML = "Windows";
-        const osOption2 = document.createElement("option");
-        osOption2.setAttribute("value", "Mac");
-        osOption2.innerHTML = "Mac";
-        osSelect.appendChild(osOption1);
-        osSelect.appendChild(osOption2);
-
-
-        const browserLabel = document.createElement("label");
-        browserLabel.setAttribute("for", "browser");
-        browserLabel.innerHTML = "Browser Name (if it's a web app): ";
-        const browserInput = document.createElement("input");
-        browserInput.setAttribute("type", "text");
-        browserInput.setAttribute("id", "browser");
-        browserInput.setAttribute("name", "browser");
-        browserInput.setAttribute("size", "25");
-        browserInput.setAttribute("placeholder", "otherwise, keep this blank")
-
-        const urlLabel = document.createElement("label");
-        urlLabel.setAttribute("for", "url");
-        urlLabel.innerHTML = "URL: ";
-        const urlInput = document.createElement("input");
-        urlInput.setAttribute("type", "text");
-        urlInput.setAttribute("id", "url");
-        urlInput.setAttribute("name", "url");
-        urlInput.setAttribute("placeholder", "or path to the app")
-
-        // Append the form fields to the form
-        const form = document.getElementById("addDiv");
-        form.appendChild(p)
-        form.appendChild(osLabel);
-        form.appendChild(osSelect);
-        form.appendChild(document.createElement("br"));
-        form.appendChild(document.createElement("br"));
-        form.appendChild(browserLabel);
-        form.appendChild(browserInput);
-        form.appendChild(document.createElement("br"));
-        form.appendChild(document.createElement("br"));
-        form.appendChild(urlLabel);
-        form.appendChild(urlInput);
-        form.appendChild(document.createElement("br"));
-        form.appendChild(document.createElement("br"));
+        // add event listener for delete button
+        const deleteButton = appElement.querySelector(".delete-app-button");
+        deleteButton.addEventListener("click", () => {
+            appElement.remove();
+        });
     });
-
 });
